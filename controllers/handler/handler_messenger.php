cb9adc1d28f65daa9ab3764998f8950049c4d026
dispxxx <contact@dispix.net>
2015-11-16 16:59:17 +0100

    Mise en place partie messenger

--- controllers/handler/handler_messenger.php
+++ controllers/handler/handler_messenger.php
@@ -1,2 +1,48 @@
 <?php
-echo "handler messenger";
+	echo "handler messenger";
+
+	/*
+	 * Field empty verify
+	 */
+	if (isset($_POST['msg_subject'])) {
+		$subject = $_POST['msg_subject'];
+	} else {
+		$errors[] = 'no_subject';
+	}
+	if (isset($_POST['msg_recipient'])) {
+		/*
+		/*	Get recipient ID if it exists
+		*/
+		$recipient = mysqli_real_escape_string($db, $_POST['msg_recipient']);
+		$result = mysqli_query($db, 'SELECT id, username
+									 FROM user
+									 WHERE username = "'.$recipient.'"');
+		if (!($recipient_array = mysqli_fetch_assoc($result))) {
+			$errors[] = 'wrong_recipient';
+		}
+	} else {
+		$errors[] = 'no_recipient';
+	}
+	if (isset($_POST['msg_content'])) {
+		$content = $_POST['msg_content'];
+	} else {
+		$errors[] = 'no_content';
+	}
+
+	if (count($errors) == 0) {
+		$id_recipient = $recipient_array['id'];
+		$id_sender = $_SESSION['id'];
+		$id_prev = 0;
+		$date_published = date("Y-m-d h:i:s");
+		$subject = mysqli_real_escape_string($db, $subject);
+		$content = mysqli_real_escape_string($db, $content);
+		if ((mysqli_query($db, 'INSERT INTO private_msg(id_recipient, id_sender, date_published, id_prev, subject, content)
+                               		VALUES ('.$id_recipient.', '.$id_sender.', "'.$date_published.'", '.$id_prev.', "'.$subject.'", "'.$content.'")'))) {
+			header('Location: ?page=messenger&status=succcess');
+			exit;
+		}
+
+	}
+
+	var_dump($errors);
+	var_dump($_POST['msg_content']);
