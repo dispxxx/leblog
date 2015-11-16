ffe22de4c9a2c9c33a0dafa37f6ebf27b80ed555
Somewher <mickaelkael12@hotmail.com>
2015-11-16 16:45:16 +0100

    hello

--- controllers/content/article/article.php
+++ controllers/content/article/article.php
@@ -1,2 +1,9 @@
 <?php
+$query = "SELECT article.id, user.username, title, date_published, content 
+FROM article
+LEFT JOIN user ON article.id_user = user.id
+ORDER BY article.id DESC LIMIT 0, 5 ";
+$resultat = mysqli_query($db, $query);
+$article = mysqli_fetch_assoc($resultat);
 require('./views/content/article/article.phtml');
+?>
