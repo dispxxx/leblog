30009626e12f283bc05e8dc5faf8e6a00061ff47
Somewher <mickaelkael12@hotmail.com>
2015-11-17 09:34:05 +0100

    db

--- init_database.php
+++ init_database.php
@@ -1,6 +1,6 @@
 <?php
 
 define("DB_HOST", "localhost");     // Serveur de la base de donnée
-define("DB_NAME", "blog_unk");      // Nom de la base de donnée
+define("DB_NAME", "projet-groupe");      // Nom de la base de donnée
 define("DB_USERNAME", "root");      // Login de la base de donnée
-define("DB_PASSWORD", "");          // Password du compte
+define("DB_PASSWORD", "troiswa");          // Password du compte
