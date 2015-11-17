222d87d5b310c047fd61921230464629b8503143
Poloch0n <zjulien67@gmail.com>
2015-11-17 15:59:43 +0100

    article_validation : ok

--- controllers/content/article_validation/article_validation.php
+++ controllers/content/article_validation/article_validation.php
@@ -1,2 +1,11 @@
 <?php
-require('./views/content/article_validation/article_validation.phtml');
+
+$retour = mysqli_query($db, 'SELECT COUNT(*) AS nb_messages FROM article WHERE date_validation = "0000-00-00 00:00:00"');
+$count = mysqli_fetch_array($retour);
+if($count['nb_messages'] > 0){
+
+    require('./views/content/article_validation/article_validation.phtml');
+}
+else{
+    require('./views/content/article_validation/article_validation_empty.phtml');
+}
--- controllers/content/home/home.php
+++ controllers/content/home/home.php
@@ -1,4 +1,10 @@
 <?php
+
+
+// On récupère le nombre total de messages
+$retour = mysqli_query($db, 'SELECT COUNT(*) AS nb_messages FROM article WHERE date_validation <> "0000-00-00 00:00:00"');
+$donnees = mysqli_fetch_array($retour);
+
 // On met dans une variable le nombre de messages qu'on veut par page
 $nombreDeMessagesParPage = 5;
 $nombreOffset = 0;
@@ -8,18 +14,15 @@ if (isset($_GET['p'])) {
 if (isset($_GET['o'])) {
     $nombreOffset = intval($_GET['o']);
 }
-
-
-// On récupère le nombre total de messages
-$retour = mysqli_query($db, 'SELECT COUNT(*) AS nb_messages FROM article');
-$donnees = mysqli_fetch_array($retour);
-$totalDesMessages = $donnees['nb_messages'];
+if ($donnees['nb_messages'] == 0) {
+    require('./views/content/home/home_empty.phtml');
+} else {
+    $totalDesMessages = $donnees['nb_messages'];
 // On calcule le nombre de pages à créer
-$nombreDePages = ceil($totalDesMessages / $nombreDeMessagesParPage);
+    $nombreDePages = ceil($totalDesMessages / $nombreDeMessagesParPage);
 // Puis on fait une boucle pour écrire les liens vers chacune des pages
-$count = 0;
+    $count = 0;
 
-require('./views/content/home/home.phtml');
-
-
-require('./views/content/home/home_pagination/home_pagination.phtml');
+    require('./views/content/home/home.phtml');
+    require('./views/content/home/home_pagination/home_pagination.phtml');
+}
