069fd2ce89359cebb471d04855c02c3626f8ee5f
Poloch0n <zjulien67@gmail.com>
2015-11-17 01:05:34 +0100

    init

--- controllers/content/article/article.php
+++ controllers/content/article/article.php
@@ -1,2 +1,14 @@
 <?php
+
+
+$query = "SELECT article.id, user.username, title, date_published, content 
+FROM article
+LEFT JOIN user ON article.id_user = user.id
+WHERE article.id = '".$_GET['id']."'
+";
+$resultat = mysqli_query($db, $query);
+$article = mysqli_fetch_assoc($resultat);
+
+
+
 require('./views/content/article/article.phtml');
--- controllers/content/home/home.php
+++ controllers/content/home/home.php
@@ -1,2 +1,25 @@
 <?php
+// On met dans une variable le nombre de messages qu'on veut par page
+$nombreDeMessagesParPage = 5;
+$nombreOffset = 0;
+if (isset($_GET['p'])) {
+    $nombreDeMessagesParPage = intval($_GET['p']);
+}
+if (isset($_GET['o'])) {
+    $nombreOffset = intval($_GET['o']);
+}
+
+
+// On récupère le nombre total de messages
+$retour = mysqli_query($db, 'SELECT COUNT(*) AS nb_messages FROM article');
+$donnees = mysqli_fetch_array($retour);
+$totalDesMessages = $donnees['nb_messages'];
+// On calcule le nombre de pages à créer
+$nombreDePages = ceil($totalDesMessages / $nombreDeMessagesParPage);
+// Puis on fait une boucle pour écrire les liens vers chacune des pages
+$count = 0;
+
 require('./views/content/home/home.phtml');
+
+
+require('./views/content/home/home_pagination/home_pagination.phtml');
