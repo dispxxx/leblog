6e0093101e75989008b3469acf6c59ea1cbc63dd
Somewher <mickaelkael12@hotmail.com>
2015-11-18 13:43:32 +0100

    maj pagination coms

--- controllers/content/article/article.php
+++ controllers/content/article/article.php
@@ -6,4 +6,30 @@ WHERE article.id = '". $_GET['id'] ."'";
 $resultat = mysqli_query($db, $query);
 $article = mysqli_fetch_assoc($resultat);
 require('./views/content/article/article.phtml');
+
+if (isset($_SESSION['id'])) 
+{
+	require('./views/content/article/article_comment_form/article_comment_form.phtml');
+}
+else
+{
+	echo 'Veuillez vous <a href="?page=login">connecter</a> pour poster un commentaire<br>';
+}
+
+$query = "SELECT COUNT(*)
+FROM comments
+LEFT JOIN article ON comments.id_article = article.id
+WHERE article.id = '". intval($_GET['id'])."'";
+$resultatCom = mysqli_query($db, $query);
+$commentaires = mysqli_fetch_assoc($resultatCom);
+if ($commentaires['COUNT(*)'] > 0)
+{
+	require('./views/content/article/article_comment_list/article_comment_list_button.phtml');
+	require('./controllers/content/article/article_comment_list/article_comment_list.php');
+	require('./views/content/article/article_comment_list/article_comment_pagination/article_comment_pagination.phtml');
+}
+else
+{
+	echo("Aucun commentaire n'a encore été posté");
+}
 ?>
--- controllers/content/article/article_comment_list/article_comment_pagination/article_comment_pagination.php
+++
@@ -1,3 +0,0 @@
-<?php 
-require('./views/content/article/article_comment_list/article_comment_pagination/article_comment_pagination.phtml');
- ?>
