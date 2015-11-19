7c986121264a90aade59d98096d25f3fa0f65a34
berserkr1 <g.coirier@epmistes.net>
2015-11-19 15:19:14 +0100

    pouet

--- controllers/content/article/article.php
+++ controllers/content/article/article.php
@@ -25,7 +25,7 @@ if ($article != NULL) {
     $commentaires = mysqli_fetch_assoc($resultatCom);
     if ($commentaires['COUNT(*)'] > 0) {
         require('./views/content/article/article_comment_list/article_comment_list_button.phtml');
-        require('./controllers/content/article/article_comment_list/article_comment_list/article_comment_list.php');
+        require('./controllers/content/article/article_comment_list/article_comment_list.php');
         require('./views/content/article/article_comment_list/article_comment_pagination/article_comment_pagination.phtml');
     }else{
         echo "aucun commentaire";
