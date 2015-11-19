e51e0940992c60fc9b23a9a9feac17d66c5d8d7a
Somewher <mickaelkael12@hotmail.com>
2015-11-19 11:03:53 +0100

    maj list utilisateur

--- controllers/content/users_view/users_view.php
+++ controllers/content/users_view/users_view.php
@@ -1,25 +1,6 @@
 <?php 
-$query = "SELECT user.id, user.surname, article.id_user AS article_author
-FROM user
-LEFT JOIN article ON user.id = article.id_user
-LIMIT 0 , 30";
+$query = "SELECT user.id, user.username
+FROM user";
 $resultat = mysqli_query($db, $query);
-
-$query = "SELECT COUNT(*)
-FROM article
-WHERE id_user = '" . $_GET['id'] . "'";
-$resultat2 = mysqli_query($db, $query);
-$nbArticles = mysqli_fetch_assoc($resultat2);
-
-if ($resultat) 
-{
-	while ($user = mysqli_fetch_assoc($resultat))
-	{
-		require('./views/content/users_view/users_view.phtml');
-	}
-}
-else 
-{
-    $errors[] = mysqli_error($db);
-}
-?>
+require('./views/content/users_view/users_view.phtml');
+ ?>
