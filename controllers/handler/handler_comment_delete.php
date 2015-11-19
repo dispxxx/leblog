854ef286bf9c80520d87d2852f49918eff5199cf
berserkr1 <g.coirier@epmistes.net>
2015-11-19 13:11:46 +0100

    gestion suppression comment

--- controllers/handler/handler_comment_delete.php
+++ controllers/handler/handler_comment_delete.php
@@ -1,2 +1,8 @@
 <?php
-echo "handler comment delete";
+	$commentToDelete = "DELETE FROM comments WHERE id='".$_POST['id']."'";
+	$commentDeleted = mysqli_query($db, $commentToDelete);
+	if ($commentDeleted) {
+	header ("Location: ?page=article&id=".intval($_GET['id']));
+	exit;
+	}
+?>
