0f17fc525500d816201f255d1721f31968092dcc
Poloch0n <zjulien67@gmail.com>
2015-11-19 15:15:50 +0100

    up stars

--- controllers/content/article/article.php
+++ controllers/content/article/article.php
@@ -9,7 +9,15 @@ if (isset($_GET['id'])) {
     $article = mysqli_fetch_assoc($resultat);
 
     if ($article != NULL) {
-
+        /*
+         *
+         * Check rate star
+         * */
+        $query = mysqli_query($db,' SELECT rating
+                    FROM star
+                    WHERE id_user = "'.$_SESSION['id'].'" AND id_article = "'.intval($_GET['id']).'"
+        ');
+        $resultatCountRate = mysqli_fetch_assoc($query);
     	require('./views/content/article/article.phtml');
 
     	if (isset($_SESSION['id'])) {
@@ -18,16 +26,6 @@ if (isset($_GET['id'])) {
     	} else {
     		require('./views/content/article/article_comment_form/article_comment_form_logout.phtml');
     	}
-        /*
-         *
-         * Check rate star
-         * */
-        $query = mysqli_query($db,' SELECT COUNT(*)
-                    FROM star
-                    WHERE id_user = "'.$_SESSION['id'].'" AND id_article = "'.intval($_GET['id']).'"
-        ');
-        $resultatCountRate = mysqli_fetch_assoc($query);
-        echo $resultatCountRate["COUNT(*)"];
 
         $query = "  SELECT COUNT(*)
                     FROM comments
