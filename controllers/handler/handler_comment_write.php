794fb66df8dc491e49fc25d5ce39b33e203efe5c
Berserkr1 <g.coirier@epmistes.net>
2015-11-20 14:05:17 +0100

    Update handler_comment_write.php

--- controllers/handler/handler_comment_write.php
+++ controllers/handler/handler_comment_write.php
@@ -4,9 +4,9 @@
 		$get_id_article = "SELECT id FROM article WHERE id='".intval($_GET['id'])."'";
 		$id_article = mysqli_query($db, $get_id_article);
 		if ($id_article) {
+			$comment = mysqli_real_escape_string($db, $_POST['comment']);
 			if (strlen($_POST['comment']) <= 510) {
 				$id_article = mysqli_fetch_array($id_article);
-				$comment = mysqli_real_escape_string($db, $_POST['comment']);
 				$commentToPost = "INSERT INTO comments (id_user,id_article,content) VALUES (".$_SESSION['id'].", ".$id_article[0].", '".$comment."')";
 				$commentPosted = mysqli_query($db, $commentToPost);
 				if ($commentPosted) {
